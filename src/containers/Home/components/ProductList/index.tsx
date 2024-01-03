80c2e3ade5c88537cf9dfd5d416bfe13a8d53c29
ustbweihy <56748102+weihyZzz@users.noreply.github.com>
2024-01-03 16:26:36 +0800

    feat: 实现自定义下拉刷新PullToRefresh

--- src/containers/Home/components/ProductList/index.tsx
+++ src/containers/Home/components/ProductList/index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import { useProducts } from '@/services/product';
-import { Grid } from 'antd-mobile';
+import { Grid, PullToRefresh } from 'antd-mobile';
 import style from './index.module.less';
 import ProductCard from '../ProductCard';
 
@@ -16,16 +16,18 @@ const ProductList = () => {
   }, []);
   return (
     <div className={style.container}>
-      <Grid
-        columns={2}
-        gap={10}
-      >
-        {data?.map((item) => (
-          <Grid.Item key={item.id}>
-            <ProductCard data={item} />
-          </Grid.Item>
-        ))}
-      </Grid>
+      <PullToRefresh>
+        <Grid
+          columns={2}
+          gap={10}
+        >
+          {data?.map((item) => (
+            <Grid.Item key={item.id}>
+              <ProductCard data={item} />
+            </Grid.Item>
+          ))}
+        </Grid>
+      </PullToRefresh>
     </div>
   );
 };
